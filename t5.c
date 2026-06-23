e40cf47cd01c8cfda3aebffc6f449a02ad008e76
Jaideep Bhati <bhatijayesh753@gmail.com>
2026-06-23 13:35:20 +0530

    1st function work

---
+++ t5.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int multiply();
+int division();
+int sub();
+
+int multiply()
+{
+    int a=2;
+    int b=5;
+    printf("multiply: %d",a*b);
+}
+
+int division()
+{
+    int c=4;
+    int d=2;
+    printf("\ndivision: %d",c/d);
+}
+
+int sub()
+{
+    int e=5;
+    int f=3;
+    printf("\nsub: %d",e-f);
+}
+
+int main()
+{   
+
+    // multiply();
+    // division();
+    // sub();
+
+
+    return 0;
+}
